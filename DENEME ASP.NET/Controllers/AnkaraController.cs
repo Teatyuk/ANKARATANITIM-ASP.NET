b954edb8e0138623e38205024f0f19b70b16118b
Ahmed Faruk Toker <ahmetfarukr@gmail.com>
2024-12-28 23:47:24 +0300

    Proje dosyası ekle.

---
+++ DENEME ASP.NET/Controllers/AnkaraController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DENEME_ASP.NET.Controllers
+{
+    public class AnkaraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult TarihiYerler()
+        {
+            return View();
+        }
+
+
+        public IActionResult Müzeler()
+        {
+            return View();
+        }
+
+
+        public IActionResult KültürelYemekler()
+        {
+            return View();
+        }
+        public IActionResult Ulaşım()
+        {
+            return View();
+        }
+        public IActionResult Üniversiteler()
+        {
+            return View();
+        }
+    }
+}
---
+++ DENEME ASP.NET/Controllers/Odev.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DENEME_ASP.NET.Controllers
+{
+    public class Odev : Controller
+    {
+        public IActionResult DENEME()
+        {
+            return View();
+        }
+    }
+}
